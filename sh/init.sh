a96bf4fb9d92daef32fe8251005534e708ec0c14
lihua <lihua@lihuadeMacBook-Pro.local>
2023-04-10 00:53:07 +0800

    new package

--- sh/init.sh
+++ sh/init.sh
@@ -46,15 +46,15 @@ echo $account > $data/account
 elif [ "$1" = "coin" ]
 then
     echo "airdrop coin"
-    scale sui coin airdrop -c $sui_coin -a 3000000000000
+    scale -g 10000000 sui coin airdrop -c $sui_coin -a 3000000000000
 elif [ "$1" = "scale" ] 
 then {
     echo "create market and account"
-    scale sui trade create_account -c $scale_coin
-    scale sui trade create_market -c $scale_coin -s 'Crypto.BTC/USD' -p $btc_price_feed -z 1 -o 2000000000 -d 'this is Crypto.BTC/USD trade market' -i 'https://bafybeibicbqm5zwxovveyxanp46njyniixuqn2ic3vv3q5n247qtfnvteu.ipfs.w3s.link/btc.svg'
-    scale sui trade create_market -c $scale_coin -s 'Crypto.ETH/USD' -p $eth_price_feed -z 1 -o 2000000000 -d 'this is Crypto.ETH/USD trade market' -i 'https://bafybeigphp4mwadmmns34bnwuavplqlbmch22w6krxwdqaemmqmtmvmsna.ipfs.w3s.link/eth.svg'
-    scale sui trade create_market -c $scale_coin -s 'Crypto.DOGE/USD' -p $doge_price_feed -z 1 -o 2000000000 -d 'this is Crypto.DOGE/USD trade market' -i 'https://bafybeiawqnf5oomm5xouukehulgjdn77vurojopixx6zlxn2tcmepbdlbe.ipfs.w3s.link/doge.svg'
-    scale sui trade add_factory_mould -n 'scale' -d 'default style' -u 'https://gateway.ipfs.io/ipfs/bafybeibckfurkark4hnob2baoayemi7fj24wyrmdct3o45s7qgwijycjyi/1797.png'
+    scale -g 10000000 sui trade create_account -c $scale_coin
+    scale -g 10000000 sui trade create_market -c $scale_coin -s 'Crypto.BTC/USD' -p $btc_price_feed -z 1 -o 2000000000 -d 'this is Crypto.BTC/USD trade market' -i 'https://bafybeibicbqm5zwxovveyxanp46njyniixuqn2ic3vv3q5n247qtfnvteu.ipfs.w3s.link/btc.svg'
+    scale -g 10000000 sui trade create_market -c $scale_coin -s 'Crypto.ETH/USD' -p $eth_price_feed -z 1 -o 2000000000 -d 'this is Crypto.ETH/USD trade market' -i 'https://bafybeigphp4mwadmmns34bnwuavplqlbmch22w6krxwdqaemmqmtmvmsna.ipfs.w3s.link/eth.svg'
+    scale -g 10000000 sui trade create_market -c $scale_coin -s 'Crypto.DOGE/USD' -p $doge_price_feed -z 1 -o 2000000000 -d 'this is Crypto.DOGE/USD trade market' -i 'https://bafybeiawqnf5oomm5xouukehulgjdn77vurojopixx6zlxn2tcmepbdlbe.ipfs.w3s.link/doge.svg'
+    scale -g 10000000 sui trade add_factory_mould -n 'scale' -d 'default style' -u 'https://gateway.ipfs.io/ipfs/bafybeibckfurkark4hnob2baoayemi7fj24wyrmdct3o45s7qgwijycjyi/1797.png'
 }
 elif [ "$1" = "open_position" ]
 then {
@@ -69,9 +69,9 @@ then {
 elif [ "$1" = "oracle" ]
 then {
     echo "oracle"
-    scale sui oracle create_price_feed -s Crypto.BTC/USD
-    scale sui oracle create_price_feed -s Crypto.ETH/USD
-    scale sui oracle create_price_feed -s Crypto.DOGE/USD
+    scale -g 10000000 sui oracle create_price_feed -s Crypto.BTC/USD
+    scale -g 10000000 sui oracle create_price_feed -s Crypto.ETH/USD
+    scale -g 10000000 sui oracle create_price_feed -s Crypto.DOGE/USD
 }
 elif [ "$1" = "deposit" ]
 then {
